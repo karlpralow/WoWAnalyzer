08addb92c0367798d7f87588f99c8bd6e6476c2c
karlpralow <karlpralow@gmail.com>
2018-05-17 12:27:33 -0400

    Fail a cause de cycle

--- src/Parser/Druid/Restoration/Modules/Features/Checklist.js
+++ src/Parser/Druid/Restoration/Modules/Features/Checklist.js
@@ -23,6 +23,7 @@ import Clearcasting from './Clearcasting';
 import Efflorescence from './Efflorescence';
 import Innervate from './Innervate';
 import Lifebloom from './Lifebloom';
+import NaturesEssence from './NaturesEssence';
 import WildGrowth from './WildGrowth';
 
 import Cultivation from '../Talents/Cultivation';
@@ -41,6 +42,7 @@ class Checklist extends CoreChecklist {
     innervate: Innervate,
     lifebloom: Lifebloom,
     manaValues: ManaValues,
+    naturesEssence: NaturesEssence,
     wildGrowth: WildGrowth,
 
     cultivation: Cultivation,
@@ -52,19 +54,6 @@ class Checklist extends CoreChecklist {
     prePotion: PrePotion,
     enchantChecker: EnchantChecker,
   };
-
-  get suggestionThresholds() {
-  return {
-    actual: this.percentBelowRecommendedCasts,
-    isGreaterThan: {
-      minor: 0.00,
-      average: 0.15,
-      major: 0.35,
-    },
-    style: 'percentage',
-  };
-}
-
 
   rules = [
     new Rule({
